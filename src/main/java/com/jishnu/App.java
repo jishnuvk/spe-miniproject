5e5704609b6fc60a6d7c7df114144c64d5f6e482
jishnuvk <jish2000@gmail.com>
2022-04-02 12:34:00 +0530

    added squareroot

--- src/main/java/com/jishnu/App.java
+++ src/main/java/com/jishnu/App.java
@@ -1,13 +1,46 @@
 package com.jishnu;
 
-/**
- * Hello world!
- *
- */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class App 
-{
+{   
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        String menu = "Choose an oparation\n";
+        menu += "1:squareRoot\n";
+        menu += "0:Exit\n";
+        
+        int choice = -1;
+        Scanner scanner = new Scanner(System.in);
+        
+        while(choice != 0){
+
+            System.out.println(menu);
+
+            try{
+                choice = scanner.nextInt();
+            }
+            catch(InputMismatchException e){
+                System.out.println("Enter a valid option");
+                continue;
+            }
+
+            if(choice == 0){
+                break;
+            }
+            else if(choice == 1){
+
+                System.out.println("Enter the number:");
+                Double n = scanner.nextDouble();
+                System.out.println(Math.sqrt(n));
+            }
+            
+            
+        }
+
+        scanner.close();
+
     }
 }
